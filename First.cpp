65956e082a04a14bd77f9efa4eab1b270572b640
Aaryan Patel <patel.aaryan336@gmail.com>
2025-01-22 22:42:40 +0530

    this is 2nd test

--- First.cpp
+++ First.cpp
@@ -6,7 +6,7 @@ int main(){
     string name ;
     int age;
     cout << "What's your name? \n";
-    getline(cin , name);
+    getline(cin  , name);
     cout << "What's yours age? \n";
     cin >> age ;
     cout << "Hello "  << name << "\n";
