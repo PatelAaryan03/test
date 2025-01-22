db18bd8efe154e7071627096fc05243d8ce655bc
Aaryan Patel <patel.aaryan336@gmail.com>
2025-01-22 22:41:06 +0530

    this is the test

---
+++ First.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    string name ;
+    int age;
+    cout << "What's your name? \n";
+    getline(cin , name);
+    cout << "What's yours age? \n";
+    cin >> age ;
+    cout << "Hello "  << name << "\n";
+    cout << "You are " << age <<  " years old." << "\n";
+
+}
